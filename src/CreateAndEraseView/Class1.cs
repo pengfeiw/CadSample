ea8922e0df5b9b7f8c420d4ec16f80768706a66e
Pengfei Wang <13671619364@sina.cn>
2019-03-11 17:33:23 +0800

    添加ControlWindow,CreateAndEraseView,zoomWindow项目

---
+++ src/CreateAndEraseView/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace CreateAndEraseView
+{
+    public class Class1
+    {
+        /// <summary>
+        /// 为图添加视图，这里视图指的是从某个角度看(俯视图，正视图，侧视图)。
+        /// </summary>
+        [CommandMethod("CreateNamedView")]
+        public static void CreateNamedView()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                ViewTable viewTable = trans.GetObject(db.ViewTableId, OpenMode.ForRead) as ViewTable;
+                if (viewTable.Has("view2") == false)
+                {
+                    ViewTableRecord newViewRecord = new ViewTableRecord();
+                    newViewRecord.Name = "view2";
+                    viewTable.UpgradeOpen();
+                    viewTable.Add(newViewRecord);
+                    trans.AddNewlyCreatedDBObject(newViewRecord, true);
+                    doc.Editor.SetCurrentView(newViewRecord);
+                }
+                trans.Commit();
+            }
+        }
+
+        [CommandMethod("EraseNamedView")]
+        public static void EraseNamedView()
+        {
+            // Get the current database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the View table for read
+                ViewTable acViewTbl;
+                acViewTbl = acTrans.GetObject(acCurDb.ViewTableId,
+                                              OpenMode.ForRead) as ViewTable;
+
+                // Check to see if the named view 'View1' exists
+                if (acViewTbl.Has("View1") == true)
+                {
+                    // Open the View table for write
+                    acViewTbl.UpgradeOpen();
+
+                    // Get the named view
+                    ViewTableRecord acViewTblRec;
+                    acViewTblRec = acTrans.GetObject(acViewTbl["View1"],
+                                                     OpenMode.ForWrite) as ViewTableRecord;
+
+                    // Remove the named view from the View table
+                    acViewTblRec.Erase();
+
+                    // Commit the changes
+                    acTrans.Commit();
+                }
+
+                // Dispose of the transaction
+            }
+        }
+    }
+}
---
+++ src/ZoomWindow/Class1.cs
@@ -0,0 +1,205 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+
+namespace ZoomWindow
+{
+    public class Class1
+    {
+        static void Zoom(Point3d pMin, Point3d pMax, Point3d pCenter, double dFactor)
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            int nCurVport = System.Convert.ToInt32(Application.GetSystemVariable("CVPORT"));
+
+            // Get the extents of the current space no points 
+            // or only a center point is provided
+            // Check to see if Model space is current
+            if (acCurDb.TileMode == true)
+            {
+                if (pMin.Equals(new Point3d()) == true &&
+                    pMax.Equals(new Point3d()) == true)
+                {
+                    pMin = acCurDb.Extmin;
+                    pMax = acCurDb.Extmax;
+                }
+            }
+            else
+            {
+                // Check to see if Paper space is current
+                if (nCurVport == 1)
+                {
+                    // Get the extents of Paper space
+                    if (pMin.Equals(new Point3d()) == true &&
+                        pMax.Equals(new Point3d()) == true)
+                    {
+                        pMin = acCurDb.Pextmin;
+                        pMax = acCurDb.Pextmax;
+                    }
+                }
+                else
+                {
+                    // Get the extents of Model space
+                    if (pMin.Equals(new Point3d()) == true &&
+                        pMax.Equals(new Point3d()) == true)
+                    {
+                        pMin = acCurDb.Extmin;
+                        pMax = acCurDb.Extmax;
+                    }
+                }
+            }
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Get the current view
+                using (ViewTableRecord acView = acDoc.Editor.GetCurrentView())
+                {
+                    Extents3d eExtents;
+
+                    // Translate WCS coordinates to DCS
+                    Matrix3d matWCS2DCS;
+                    matWCS2DCS = Matrix3d.PlaneToWorld(acView.ViewDirection);
+                    matWCS2DCS = Matrix3d.Displacement(acView.Target - Point3d.Origin) * matWCS2DCS;
+                    matWCS2DCS = Matrix3d.Rotation(-acView.ViewTwist,
+                                                   acView.ViewDirection,
+                                                   acView.Target) * matWCS2DCS;
+
+                    // If a center point is specified, define the min and max 
+                    // point of the extents
+                    // for Center and Scale modes
+                    if (pCenter.DistanceTo(Point3d.Origin) != 0)
+                    {
+                        pMin = new Point3d(pCenter.X - (acView.Width / 2),
+                                           pCenter.Y - (acView.Height / 2), 0);
+
+                        pMax = new Point3d((acView.Width / 2) + pCenter.X,
+                                           (acView.Height / 2) + pCenter.Y, 0);
+                    }
+
+                    // Create an extents object using a line
+                    using (Line acLine = new Line(pMin, pMax))
+                    {
+                        eExtents = new Extents3d(acLine.Bounds.Value.MinPoint,
+                                                 acLine.Bounds.Value.MaxPoint);
+                    }
+
+                    // Calculate the ratio between the width and height of the current view
+                    double dViewRatio;
+                    dViewRatio = (acView.Width / acView.Height);
+
+                    // Tranform the extents of the view
+                    matWCS2DCS = matWCS2DCS.Inverse();
+                    eExtents.TransformBy(matWCS2DCS);
+
+                    double dWidth;
+                    double dHeight;
+                    Point2d pNewCentPt;
+
+                    // Check to see if a center point was provided (Center and Scale modes)
+                    if (pCenter.DistanceTo(Point3d.Origin) != 0)
+                    {
+                        dWidth = acView.Width;
+                        dHeight = acView.Height;
+
+                        if (dFactor == 0)
+                        {
+                            pCenter = pCenter.TransformBy(matWCS2DCS);
+                        }
+
+                        pNewCentPt = new Point2d(pCenter.X, pCenter.Y);
+                    }
+                    else // Working in Window, Extents and Limits mode
+                    {
+                        // Calculate the new width and height of the current view
+                        dWidth = eExtents.MaxPoint.X - eExtents.MinPoint.X;
+                        dHeight = eExtents.MaxPoint.Y - eExtents.MinPoint.Y;
+
+                        // Get the center of the view
+                        pNewCentPt = new Point2d(((eExtents.MaxPoint.X + eExtents.MinPoint.X) * 0.5),
+                                                 ((eExtents.MaxPoint.Y + eExtents.MinPoint.Y) * 0.5));
+                    }
+
+                    // Check to see if the new width fits in current window
+                    if (dWidth > (dHeight * dViewRatio)) dHeight = dWidth / dViewRatio;
+
+                    // Resize and scale the view
+                    if (dFactor != 0)
+                    {
+                        acView.Height = dHeight * dFactor;
+                        acView.Width = dWidth * dFactor;
+                    }
+
+                    // Set the center of the view
+                    acView.CenterPoint = pNewCentPt;
+
+                    // Set the current view
+                    acDoc.Editor.SetCurrentView(acView);
+                }
+
+                // Commit the changes
+                acTrans.Commit();
+            }
+        }
+
+        [CommandMethod("ZoomWindow")]
+        public static  void ZoomWindow()
+        {
+            // Zoom to a window boundary defined by 1.3,7.8 and 13.7,-2.6
+            Point3d pMin = new Point3d(1.3, 7.8, 0);
+            Point3d pMax = new Point3d(13.7, -2.6, 0);
+
+            Zoom(pMin, pMax, new Point3d(), 1);
+        }
+
+        [CommandMethod("ZoomScale")]
+        static public void ZoomScale()
+        {
+            // Get the current document
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+
+            // Get the current view
+            using (ViewTableRecord acView = acDoc.Editor.GetCurrentView())
+            {
+                // Get the center of the current view
+                Point3d pCenter = new Point3d(acView.CenterPoint.X,
+                                              acView.CenterPoint.Y, 0);
+
+                // Set the scale factor to use
+                double dScale = 2;
+
+                // Scale the view using the center of the current view
+                Zoom(new Point3d(), new Point3d(), pCenter, 1 / dScale);
+            }
+        }
+
+        [CommandMethod("ZoomCenter")]
+        static public void ZoomCenter()
+        {
+            // Center the view at 5,5,0
+            Zoom(new Point3d(), new Point3d(), new Point3d(5, 5, 0), 1);
+        }
+
+        [CommandMethod("ZoomExtents")]
+        static public void ZoomExtents()
+        {
+            // Zoom to the extents of the current space
+            Zoom(new Point3d(), new Point3d(), new Point3d(), 1.01075);
+        }
+
+        [CommandMethod("ZoomLimits")]
+        static public void ZoomLimits()
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Zoom to the limits of Model space
+            Zoom(new Point3d(acCurDb.Limmin.X, acCurDb.Limmin.Y, 0),
+                 new Point3d(acCurDb.Limmax.X, acCurDb.Limmax.Y, 0),
+                 new Point3d(), 1);
+        }
+    }
+}
