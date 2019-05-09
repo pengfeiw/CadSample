d8fefad9b553a6c00e16d5ea618c1a230ab02295
Pengfei Wang <13671619364@sina.cn>
2019-05-09 11:10:48 +0800

    Add MoveLayout project

---
+++ src/MoveLayout/MoveLayout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace MoveLayout
+{
+    public class MoveLayout
+    {
+        /// <summary>
+        /// 切换到innerBlock所在的layout（布局空间或者模型空间）
+        /// </summary>
+        public static  void SwitchToLayout(BlockReference innerBlock, Transaction tr)
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            LayoutManager acLayoutMgr = LayoutManager.Current;
+
+            BlockTableRecord blkTblRec = tr.GetObject(innerBlock.OwnerId, OpenMode.ForRead) as BlockTableRecord;
+            Layout objLayout = tr.GetObject(blkTblRec.LayoutId, OpenMode.ForRead) as Layout;
+            if (objLayout != null)
+            {
+                acLayoutMgr.CurrentLayout = objLayout.LayoutName;
+            }
+        }
+    }
+}
